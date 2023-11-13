d63cbd474c63972c8598c1bbd7c8cb3fb67548cb
Fio <leukert.fio@gmail.com>
2023-11-13 16:41:32 +0100

    start implementing concrete audio and instructions

---
+++ game/sound/sound.gd
@@ -0,0 +1,47 @@
+extends Node
+
+const BGM_SLOW_DEEP_BREATHS = "res://game/sound/CØL - Slow, Deep Breaths - 03 Slow, Deep Breaths.ogg"
+
+var rampup_time := 7.0
+
+func _ready() -> void:
+	set_background_music(BGM_SLOW_DEEP_BREATHS, 1.0)
+
+func set_background_music(key: String, _rampup_time := rampup_time):
+	rampup_time = _rampup_time
+	var t = get_tree().create_tween()
+	t.tween_property($BGMPlayer, "volume_db", -80, _rampup_time)
+	t.connect("finished", ramp_up_background_music)
+	
+	var timer = get_tree().create_timer(_rampup_time)
+	await timer.timeout
+	$BGMPlayer.stream = load(key)
+	$BGMPlayer.playing = true
+
+func ramp_up_background_music():
+	var t = get_tree().create_tween()
+	t.tween_property($BGMPlayer, "volume_db", 0, rampup_time)
+
+# idk TODO ig
+func play(soundName:String, randomPitch := true) -> AudioStreamPlayer:
+	var s = get(soundName)
+	if s is Array:
+		if s.size() == 0:
+			prints("no sounds in array ", soundName)
+			return null
+		randomize()
+		s = s[randi_range(0, s.size() - 1)]
+	if s:
+		var p = AudioStreamPlayer.new()
+		add_child(p)
+		p.volume_db = 4
+		p.connect("finished", p.queue_free)
+		p.stream = s
+		p.play()
+		if randomPitch:
+			randomize()
+			p.pitch_scale = randf_range(0.8, 1 / 0.8)
+		return p
+	else:
+		print("Sound \"" + soundName + "\" is not defined") 
+		return null
--- src/InstructionHandlerInherit.gd
+++ src/InstructionHandlerInherit.gd
@@ -1,5 +1,8 @@
 extends InstructionHandler
 
+signal request_background_change(background_name: String)
+signal make_screen_black(hide_characters: bool, new_background: String, attack: float, release: float)
+
 func execute(instruction_name, args):
 	prints("EXECUTING ", instruction_name, " WITH ARGS ", args)
 	match instruction_name:
@@ -8,3 +11,24 @@ func execute(instruction_name, args):
 		"hide-all-characters":
 			for c in get_tree().get_nodes_in_group("Character"):
 				c.visible = false
+			
+		"black-fade-in-out":
+			var hide_characters : bool = args.get("hide-characters", "") == "true"
+#			if hide_characters:
+#				for c in get_tree().get_nodes_in_group("Character"):
+#					c.visible = false
+			
+			var new_background : String = args.get("new-background", "")
+			#emit_signal("request_background_change", new_background)
+			
+			var attack = 5.0
+			if not args.get("attack", "").is_empty():
+				attack = float(args.get("attack"))
+			var release = 5.0
+			if not args.get("release", "").is_empty():
+				release = float(args.get("release"))
+			
+			emit_signal("make_screen_black", hide_characters, new_background, attack, release)
+			
+		"set-background":
+			emit_signal("request_background_change", args.get("background-name", ""))
